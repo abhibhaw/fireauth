f475ffed112ed4ea144309b47725e871611d7198
abhibhaw <39991296+abhibhaw@users.noreply.github.com>
2021-12-21 02:59:42 +0530

    feat: Adds profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { Box, TextField, Typography } from "@mui/material";
+import { CommonButton } from "components";
+
 export default function Profile() {
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
   return (
     <div className="page-container">
-      <h1>Profile Page</h1>
+      <Typography variant="h3" sx={{ m: 4 }}>
+        Profile
+      </Typography>
+      <Box component="form" sx={{ m: 2 }} autoComplete="off">
+        <img
+          height="150"
+          width="150"
+          src="https://avatars.githubusercontent.com/u/39991296?v=4"
+          alt="User Avatar"
+        />
+        <TextField
+          id="name"
+          fullWidth
+          required
+          label="Your Name"
+          variant="outlined"
+          name="name"
+          sx={{ m: 2 }}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <TextField
+          fullWidth
+          required
+          id="bio"
+          label="Your Bio"
+          variant="outlined"
+          name="bio"
+          sx={{ m: 2 }}
+          onChange={(e) => {
+            setBio(e.target.value);
+          }}
+        />
+        <CommonButton title="Save" handleClick={() => console.log(name, bio)} />
+      </Box>
     </div>
   );
 }
